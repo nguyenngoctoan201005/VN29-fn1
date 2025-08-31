0db6a05a4afc8b5c16ceef6bb04a6dac4e366c9c
nguyenngoctoan201005 <nguyenngoctoan201005@gmail.com>
2025-08-31 22:20:54 +0700

    Set up HomePage

--- Countries/src/App.jsx
+++ Countries/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { Header } from './header/Header.jsx'
 import { Nav } from './header/Nav.jsx'
-
+import { HomePage } from './pages/HomePage.jsx'
 function App() {
 
   return (
     <>
       <Header />
       <Nav />
+      <HomePage />
     </>
   )
 }
---
+++ Countries/src/pages/HomePage.jsx
@@ -0,0 +1,281 @@
+import './homepage.css';
+
+export function HomePage() {
+    return (
+        <main>
+            <section className="imgintro">
+                <div className="imgintro-img1">
+                    <img src="img/1.png" alt="" />
+                </div>
+                <div className="imgintro-img2">
+                    <img src="img/2.png" alt="" />
+                </div>
+            </section>
+
+            <section className="cpn">
+                <div className="cpn-tt">
+                    <div className="cpn-tt-txt">
+                        ASIA
+                    </div>
+                    <div className="cpn-tt-xem">
+                    Xem tất cả
+                    </div>
+                </div>
+                <div className="cpn-cont">
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" /> 
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+            <section className="cpn">
+                <div className="cpn-tt">
+                    <div className="cpn-tt-txt">
+                        ASIA
+                    </div>
+                    <div className="cpn-tt-xem">
+                    Xem tất cả
+                    </div>
+                </div>
+                <div className="cpn-cont">
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" /> 
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+            <section className="cpn">
+                <div className="cpn-tt">
+                    <div className="cpn-tt-txt">
+                        ASIA
+                    </div>
+                    <div className="cpn-tt-xem">
+                    Xem tất cả
+                    </div>
+                </div>
+                <div className="cpn-cont">
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" /> 
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="cpn-cont-det">
+                        <div className="cpn-cont-det-img"><img src="img/Co-Vietnam.png" alt="" /></div>
+                        <div className="cpn-cont-info">
+                            <div>
+                                <div>Name : VietNam</div>
+                                <div>cca2 : VN </div>
+                                <div>cioc : VIE </div>
+                            </div>
+                            <div className="cpn-cont-info-heart">
+                                <img src="img/heart.png" alt="" />
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+        </main>
+    );
+}
